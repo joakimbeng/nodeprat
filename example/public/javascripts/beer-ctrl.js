5f34d3e87b43392d41fbf1b87222063e5e3ed624
Joakim Bengtson <joakim.bengtson@lanemarknad.se>
2013-04-24 09:22:18 +0200

    Adding more advanced sort

--- example/public/javascripts/beer-ctrl.js
+++ example/public/javascripts/beer-ctrl.js
@@ -2,4 +2,10 @@ app.controller('BeerCtrl', function ($scope, $http) {
   $http.get('/beers').success(function (beers) {
     $scope.beers = beers;
   });
+
+  $scope.setSort = function (by) {
+    if (by === $scope.sortBy)
+      $scope.reverse = !!!$scope.reverse;
+    $scope.sortBy = by;
+  };
 });
